c1aef447987887a6cc68b750d5ced8a8660fee21
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-12-21 15:42:19 +0100

    updated

---
+++ src/stringLength.js
@@ -0,0 +1,10 @@
+function stringLength(string) {
+  return string.length;
+}
+
+// Example usage:
+console.log(stringLength('hello')); // Output: 5
+console.log(stringLength('abcdef')); // Output: 6
+console.log(stringLength('')); // Output: 0
+
+module.exports = stringLength;
